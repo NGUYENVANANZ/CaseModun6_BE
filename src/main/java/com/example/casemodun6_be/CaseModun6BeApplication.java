91739f38646d71edc8ed631a00084d46ed9f29dd
nguyenhaivanz <haivan090824@gmail.com>
2023-01-03 23:44:16 +0700

    add

--- src/main/java/com/example/casemodun6_be/CaseModun6BeApplication.java
+++ src/main/java/com/example/casemodun6_be/CaseModun6BeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CaseModun6BeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CaseModun6BeApplication.class, args);
     }
-
 }
