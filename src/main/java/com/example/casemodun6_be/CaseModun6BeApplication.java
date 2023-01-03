86313b78d79eebe66543531e5c90853072dea94e
Nguyễn Văn An <vanankv123@gmail.com>
2023-01-03 15:05:57 +0700

    add

---
+++ src/main/java/com/example/casemodun6_be/CaseModun6BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.casemodun6_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseModun6BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseModun6BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casemodun6_be/CaseModun6BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casemodun6_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseModun6BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
